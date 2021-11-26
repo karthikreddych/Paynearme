da3b158c58bd239738a1e19e83f983e3575b1972
Ashok <ashok.v@reflectionsinfos.com>
2021-11-26 17:57:55 +0530

    lombok changes

--- src/main/java/com/lendingclub/smsbe/domain/SbtId.java
+++ src/main/java/com/lendingclub/smsbe/domain/SbtId.java
@@ -18,7 +18,7 @@ package com.lendingclub.smsbe.domain;
 import java.util.Objects;
 
 import javax.persistence.*;
-//import lombok.*;
+import lombok.Data;
 
 /**
  * @author Greg Turnquist
@@ -26,7 +26,7 @@ import javax.persistence.*;
 // tag::code[]
 @Entity //(1)
 @Table(name="sbt_id")
-//@Data
+@Data
 public class SbtId {
 
 	private @Id @GeneratedValue Long id; //(2)
@@ -43,88 +43,6 @@ public class SbtId {
 		this.smsId = smsId;
 	}
 	
-//	@Override
-//	public boolean equals(Object o) {
-//		if (this == o) return true;
-//		if (o == null || getClass() != o.getClass()) return false;
-//		Sbt_id sbt_id = (Sbt_id) o;
-//		return Objects.equals(id, sbt_id.id) &&
-//			Objects.equals(template_id, sbt_id.template_id) &&
-//			Objects.equals(sms_id, sbt_id.sms_id);
-//	}
-//
-//	@Override
-//	public int hashCode() {
-//
-//		return Objects.hash(id, template_id, sms_id);
-//	}
-//
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	public String gettemplate_id() {
-//		return template_id;
-//	}
-//
-//	public void settemplate_ide(String template_id) {
-//		this.template_id = template_id;
-//	}
-//
-//	public String getsms_id() {
-//		return sms_id;
-//	}
-//
-//	public void setsms_id(String sms_id) {
-//		this.sms_id = sms_id;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "sbt_id{" +
-//			"id=" + id +
-//			", template_id='" + template_id + '\'' +
-//			", sms_id='" + sms_id + '\'' +
-//
-//			'}';
-//	}
-//
-//	public String getTemplate_id() {
-//		return template_id;
-//	}
-//
-//	public void setTemplate_id(String template_id) {
-//		this.template_id = template_id;
-//	}
-//
-//	public String getSms_id() {
-//		return sms_id;
-//	}
-//
-//	public void setSms_id(String sms_id) {
-//		this.sms_id = sms_id;
-//	}
-
-
-	public String getTemplateId() {
-		return templateId;
-	}
-
-	public void setTemplateId(String templateId) {
-		this.templateId = templateId;
-	}
-
-	public String getSmsId() {
-		return smsId;
-	}
-
-	public void setSmsId(String smsId) {
-		this.smsId = smsId;
-	}
 }
 	
 	
