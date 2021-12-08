f180598af8c042825fa561f1554b75a426a637fd
Tabreze <career.karimee@gmail.com>
2021-12-08 18:48:24 +0530

    Modified Config

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -95,8 +95,8 @@ define([
             "to": "{Contact.Attribute.SBT.Contact}}", //<----This should map to your data extension name and phone number column
 			//console.log("Contact number from DE: ", "{{Contact.Attribute.SBT.Contact}}");
         };
-		//console.log("Contact number from DE: ", "{{Contact.Attribute.SBT.Contact}}");
-		console.log("Contact number from DE: "+JSON.stringify(Contact.Attribute.SBT.Contact));
+		console.log("Contact number from DE: ", "{{Contact.Attribute.SBT.Contact}}");
+		//console.log("Contact number from DE: "+JSON.stringify(Contact.Attribute.SBT.Contact));
 		
         payload['metaData'].isConfigured = true;
 
