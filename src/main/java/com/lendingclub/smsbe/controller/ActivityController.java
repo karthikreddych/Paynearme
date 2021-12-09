63f5b246d80e803c2fe573d0d41d3a26153aa700
Tabreze <career.karimee@gmail.com>
2021-12-09 19:18:24 +0530

    added @CrossOrigin in controller

--- src/main/java/com/lendingclub/smsbe/controller/ActivityController.java
+++ src/main/java/com/lendingclub/smsbe/controller/ActivityController.java
@@ -9,27 +9,27 @@ import java.util.Map;
 @RestController
 @RequestMapping("/activity")
 public class ActivityController {
-
+	@CrossOrigin
     @PostMapping("/save")
     public ResponseEntity<String> save (@RequestBody Map<String, Object> payload) {
         return new ResponseEntity<>("Save", HttpStatus.OK);
     }
-
+	@CrossOrigin
     @PostMapping("/publish")
     public ResponseEntity<String> publish (@RequestBody Map<String, Object> payload) {
         return new ResponseEntity<>("Publish", HttpStatus.OK);
     }
-
+	@CrossOrigin
     @PostMapping("/validate")
     public ResponseEntity<String> validate (@RequestBody Map<String, Object> payload) {
         return new ResponseEntity<>("Validate", HttpStatus.OK);
     }
-
+	@CrossOrigin
     @PostMapping("/stop")
     public ResponseEntity<String> stop (@RequestBody Map<String, Object> payload) {
         return new ResponseEntity<>("Stop", HttpStatus.OK);
     }
-
+	@CrossOrigin
     @PostMapping("/execute")
     public ActivityResult execute (@RequestBody ExecutePayload payload) {
         for (Map<String, String> inArgument : payload.getInArguments()) {
