8cd1e74a5ba42e16430c562033599667bbba3420
Tabreze <career.karimee@gmail.com>
2022-01-07 20:24:43 +0530

    updated all files from MBO to SBT

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -13,7 +13,7 @@ define([
     {"key": "step1", "label": "MBO Gayeway Template and SMS ID Selection	"}
     ];
     var currentStep = steps[0].key;
-	//var authTokens = {};
+	var authTokens = {};
     $(window).ready(onRender);
     
     try {
@@ -28,6 +28,7 @@ define([
     }
 
     function onRender() {
+	//debugger
         try {
         // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
@@ -40,6 +41,7 @@ define([
     }
 
   function initialize(data) {
+	//debugger
         //console.log("***Initialize  " + data);
         if (data) {
             payload = data;
@@ -70,41 +72,67 @@ define([
                })
         });
 
+   
         connection.trigger('updateButton', {
             button: 'next',
-            text: 'done',
+            text: 'Done',
             visible: true
-        });
-    } catch(err) {
+       
+    });
+}   catch(err) {
          throw(err);
        // console.log(err);
     }
-
-    }
+}
+   
 
     function onGetTokens (tokens) {
+	//debugger
         // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-        //console.log("Tokens function: "+JSON.stringify(tokens));
+        console.log("Tokens function: "+JSON.stringify(tokens));
         //authTokens = tokens;
-        console.log(tokens);
+        //console.log(tokens);
         authTokens = tokens;
 
     }
     
 
     function onGetEndpoints (endpoints) {
+	//debugger
         // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-        //console.log("Get End Points function: "+JSON.stringify(endpoints));
-        console.log(endpoints);
+        console.log("Get End Points function: "+JSON.stringify(endpoints));
+        //console.log(endpoints);
     }
 
     function save() {
+	debugger
         try {
 		//alert($('#SMSid').val());
-		console.log("***Calling save function: ");
-        var SMSidValue = $('#SMSid').val();
+		//console.log("***Calling save function: ");
+		var SMSidValue = $('#SMSid').val();
         var TemplateIDValue = $('#TemplateID').val();
-        
+
+
+         if( SMSidValue === "" || TemplateIDValue === ""){
+			//functionAlert()
+			//Swal.fire("Select IDs from the Dropdown");
+			//swal("Alert!", "Select IDs from the Dropdown");
+			//alert("Select IDs from the Dropdown");
+			//var element = document.createElement("div");
+    		//	element.appendChild(document.createTextNode('Select IDs from the Dropdown'));
+    		//	document.getElementById('step1').appendChild(element);
+				//alert("Select IDs from the Dropdown");
+				
+            //throw "exit"
+			//sleep(1);
+			
+			document.getElementById("step2").innerHTML = "Select ID from the Dropdown!";
+			return;
+            }
+            		
+			
+	    //payload['metaData'].isConfigured = true;
+		//payload.name = name;
         payload['arguments'].execute.inArguments = [{
             "SMSid_Value": SMSidValue,
             "TemplateID_Value": TemplateIDValue,
@@ -116,7 +144,7 @@ define([
 			"businessUnit": "{{Contact.Attribute.SMS.businessUnit}}",
 			"scheduleDate": "{{Contact.Attribute.SMS.scheduleDate}}",
 			"vendor": "{{Contact.Attribute.SMS.vendor}}",
-            "to": "{{Contact.Attribute.SMS.Contact}}" //<----This should map to your data extension name and phone number column
+            "Contact": "{{Contact.Attribute.SMS.Contact}}" //<----This should map to your data extension name and phone number column
 			
 		
         }];
@@ -126,18 +154,79 @@ define([
 
         console.log("***Payload on SAVE function: " +JSON.stringify(payload));
         connection.trigger('updateActivity', payload);
-        return 'Success';
+        //return 'Success';
         } catch(err) {
             documnet.getElement("error").style.display = "block";
             documnet.getElement("error").innerHtml = err;
         }
-		fetch('https://demo-default.uw2.customer-messaging-gateway-nprd.lendingcloud.us/api/customer-messaging-gateway/v1/message', {
-  		method: "POST",
-  		body: JSON.stringify(payload['arguments'].execute.inArguments),
-  		headers: {"Content-type": "application/json; charset=UTF-8"}
-		})
-		.then(response => response.json()).catch(err => console.log(err)) 
-        .then(json => console.log(json)).catch(err => console.log(err)); 
-    }                    
+
+
+	/*fetch('https://mc-260crls51zy9yd64d27td22t8.auth.marketingcloudapis.com/v2/token', 
+	{
+		  
+	 method: "POST",
+    // headers: {"Content-Type": "application/json; charset=utf8","Access-Control-Allow-Origin": "https://mc.s8.exacttarget.com","Access-Control-Allow-Credentials": "true"},   
+	headers: { 'Content-type': 'application/json'},
+	mode: 'no-cors',
+		
+       body: JSON.stringify(
+   		{
+    "grant_type": "client_credentials",
+    "client_id": "ca1xp4ph65dl9nxfgcbnjelk",
+    "client_secret": "5B4zhj2pTWzvjAEqImLPrttU",
+    "account_id": "517005233"
+	})
+    
+		}) 
+	.then(response => response.json()) 
+    .then(json => {
+     if(json.statusCode >= 300) { console.log("this is error")
+     } else {
+     console.log("this is success")
+      }
+    
+     }).catch(err => console.log(err));
+    
+	
+	fetch('https://mc-260crls51zy9yd64d27td22t8.rest.marketingcloudapis.com/hub/v1/dataeventsasync/key:AFE77857-1B91-49A9-96B6-C201929888D5/rowset', 
+	{
+	  
+	 method: "POST",
+    //headers: {"Content-type": "application/json, charset=UTF-8",'Authorization': "token", 'Access-Control-Allow-Origin': '*',"Access-Control-Allow-Credentials": "true"},   
+	headers: { "Content-Type": "text/html",'Authorization': "token","Access-Control-Allow-Origin": '*' },	
+	mode: 'no-cors',
+		
+       body: JSON.stringify(
+   		{
+        "keys": {
+            "LoanIDs": "{{Contact.Attribute.SMS.loanId}}"
+        },
+        "values": {
+            "Template_IDs": TemplateIDValue,
+            "SMS_IDs": SMSidValue
+        }
+    })
+		}) 
+	.then(response => response.json()) 
+    .then(json => {
+     if(json.statusCode >= 300) { console.log("this is error")
+     } else {
+     console.log("this is success")
+      }
+    
+     }).catch(err => console.log(err));*/
+    
+	console.log("SMS ID: " +JSON.stringify(SMSidValue));
+	console.log("Template ID: " +JSON.stringify(TemplateIDValue));
+	console.log("Loan ID: " +JSON.stringify("{{Contact.Attribute.SMS.loanId}}"));}
+
+
+	//	fetch('https://demo-default.uw2.customer-messaging-gateway-nprd.lendingcloud.us/api/customer-messaging-gateway/v1/message', {
+  	//	method: "POST",
+  //		body: JSON.stringify(payload['arguments'].execute.inArguments),
+  //		headers: {"Content-type": "application/json; charset=UTF-8"}
+//		})
+	//	.then(response => response.json()).catch(err => console.log(err)) 
+   //     .then(json => console.log(json)).catch(err => console.log(err)); 
 
 });
