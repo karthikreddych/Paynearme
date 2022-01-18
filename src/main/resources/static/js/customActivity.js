6c957c5394af58ec0e9ea23d6990a761251ea485
Tabreze <mkarimee@lendingclub.com>
2022-01-18 14:26:14 +0530

    Soap api for DA records retrival

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -230,7 +230,7 @@ define([
      }).catch(err => console.log(err));*/
     
 	
-	fetch ('https://mc-260crls51zy9yd64d27td22t8.rest.marketingcloudapis.com/data/v1/customobjectdata/key/AFE77857-1B91-49A9-96B6-C201929888D5', 
+	fetch ('https://mc-260crls51zy9yd64d27td22t8.rest.marketingcloudapis.com/data/v1/customobjectdata/key/AFE77857-1B91-49A9-96B6-C201929888D5/rowset?$filter=EmailAddress%20eq%20','im.tabrez@gmail.com', 'HTTP/1.1', 
 	{
 	 
 	 method: "GET",
