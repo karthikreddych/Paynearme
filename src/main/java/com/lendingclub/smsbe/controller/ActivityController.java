6a3a36bd8ad7cf325c0cc79a6aacee4535dd8d42
Tabreze <mkarimee@lendingclub.com>
2022-01-21 18:32:01 +0530

    modify config

--- src/main/java/com/lendingclub/smsbe/controller/ActivityController.java
+++ src/main/java/com/lendingclub/smsbe/controller/ActivityController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-//@RequestMapping("/activity")
+@RequestMapping("/activity")
 public class ActivityController {
 	@CrossOrigin
     @PostMapping("/save")
