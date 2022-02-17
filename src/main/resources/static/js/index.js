dcc1f60e9f3176c8fb3f00d4c28c9a081bee18b1
Mohammad Tabreze Karimee <mohammadtk@reflectionsinfos.com>
2022-02-17 18:27:57 +0530

    Added all new code with LC liberaries

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,6 @@
 const userAction = async () => {
     console.log('reaching')
-  const response = await fetch('/sbtIds');
+  const response = await fetch('api/sbtIds');
   const myJson = await response.json(); //extract JSON from the http response
   // do something with myJson
   let data = myJson;
