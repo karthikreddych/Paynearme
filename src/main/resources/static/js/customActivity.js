0a04341511771737d111814a9fb145c31ed1260c
karthikreddych <52830050+karthikreddych@users.noreply.github.com>
2022-02-23 19:02:55 +0530

    Update customActivity.js

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -208,8 +208,8 @@ save();
     	//payload['arguments'].execute = payload['arguments'].execute || {};
     	
         payload['arguments'].execute.inArguments = [{
-            "TemplateName_Value": TemplateNameValue,
-            "TemplateID_Value": TemplateIDValue,
+            "TemplateName_Value": "TemplateNameValue",
+            "TemplateID_Value": TemplateIDValue
             
             /*"loanId": "{{Contact.Attribute." + eventDefinitionKey+".\"loanId\"}}",
            "eventType": "{{Contact.Attribute." + eventDefinitionKey+".\"eventType\"}}",
