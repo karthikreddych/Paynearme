5c6c640e53db99cb66d45e7743ae871772326855
karthikreddych <52830050+karthikreddych@users.noreply.github.com>
2022-02-24 12:05:57 +0530

    Update customActivity.js

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -215,24 +215,7 @@ save();
         payload['arguments'].execute.inArguments = [{
             "TemplateName_Value": TemplateNameValue,
             "TemplateID_Value": TemplateIDValue,
-            
-            /*"loanId": "{{Contact.Attribute." + eventDefinitionKey+".\"loanId\"}}",
-           "eventType": "{{Contact.Attribute." + eventDefinitionKey+".\"eventType\"}}",
-            "communicationChannel": "{{Contact.Attribute." + eventDefinitionKey+".\"communicationChannel\"}}",
-           "primaryActorId": "{{Contact.Attribute." + eventDefinitionKey+".\"primaryActorId\"}}",
-          "businessUnit": "{{Contact.Attribute." + eventDefinitionKey+".\"businessUnit\"}}",
-           "scheduleDate": "{{Contact.Attribute." + eventDefinitionKey+".\"scheduleDate\"}}",
-            "vendor": "{{Contact.Attribute." + eventDefinitionKey+".\"vendor\"}}",
-            "EmailAddress": "{{Contact.Attribute." + eventDefinitionKey+".\"EmailAddress\"}}",
-            "ContactNo": "{{Contact.Attribute." + eventDefinitionKey+".\"ContactNo\"}}",
-            "Status": "{{Contact.Attribute." + eventDefinitionKey+".\"Status\"}}",
-            "FirstName": "{{Contact.Attribute." + eventDefinitionKey+".\"FirstName\"}}",
-            "LastName": "{{Contact.Attribute." + eventDefinitionKey+".\"LastName\"}}",
-            "CountryCode": "{{Contact.Attribute." + eventDefinitionKey+".\"CountryCode\"}}",
-            */
-            
-         
-			"loanId": "{{Contact.Attribute.SMS.loanId}}",
+            "loanId": "{{Contact.Attribute.SMS.loanId}}",
 			"eventType": "{{Contact.Attribute.SMS.eventType}}",
 			"communicationChannel": "{{Contact.Attribute.SMS.communicationChannel}}",
 			"primaryActorId": "{{Contact.Attribute.SMS.primaryActorId}}",
@@ -241,12 +224,9 @@ save();
 			"vendor": "{{Contact.Attribute.SMS.vendor}}",
             "contacts": "{{Contact.Attribute.SMS.contacts}}", 
             "emailaddress": "{{Contact.Attribute.SMS.emailaddress}}", 
-            //"status": "{{Contact.Attribute.SMS.status}}",
-            //"FirstName": "{{Contact.Attribute.SMS.FirstName}}",
-            //"LastName": "{{Contact.Attribute.SMS.LastName}}",
             "countrycode": "{{Contact.Attribute.SMS.countrycode}}",
-			 "messageContent": "{{Contact.Attribute.SMS.messageContent}}",
-			 "messageParams": "{{Contact.Attribute.SMS.messageParams}}",
+			"messageContent": "{{Contact.Attribute.SMS.messageContent}}",
+			"messageParams": "{{Contact.Attribute.SMS.messageParams}}",
 			"doNotCheckDNC": "{{Contact.Attribute.SMS.doNotCheckDNC}}",
         }];
         
