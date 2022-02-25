8f8509673288483de78f97e6cdc4235c9db5c1d0
karthikreddych <52830050+karthikreddych@users.noreply.github.com>
2022-02-25 21:24:13 +0530

    Update customActivity.js

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -204,10 +204,27 @@ save();
 		var TemplateNameValue = $('#TemplateName').val();
         var TemplateIDValue = $('#TemplateID').val();
        
+	   payload['arguments'].execute.inArguments = [];
 	   
 	   payload.inArguments.forEach(loanId => {
 	   
-	   payload['arguments'].execute.inArguments = [];
+	  payload['arguments'].execute.inArguments.push({
+            "TemplateName_Value": TemplateNameValue,
+            "TemplateID_Value": TemplateIDValue,
+			"loanId": "{{Contact.Attribute.SMS.loanId}}",
+			"eventType": "{{Contact.Attribute.SMS.eventType}}",
+			"communicationChannel": "{{Contact.Attribute.SMS.communicationChannel}}",
+			"primaryActorId": "{{Contact.Attribute.SMS.primaryActorId}}",
+			"businessUnit": "{{Contact.Attribute.SMS.businessUnit}}",
+			"scheduleDate": "{{Contact.Attribute.SMS.scheduleDate}}",
+			"vendor": "{{Contact.Attribute.SMS.vendor}}",
+            "contacts": "{{Contact.Attribute.SMS.contacts}}", 
+            "emailaddress": "{{Contact.Attribute.SMS.emailaddress}}", 
+            "countrycode": "{{Contact.Attribute.SMS.countrycode}}",
+			"messageContent": "{{Contact.Attribute.SMS.messageContent}}",
+			"messageParams": "{{Contact.Attribute.SMS.messageParams}}",
+			"doNotCheckDNC": "{{Contact.Attribute.SMS.doNotCheckDNC}}", 
+        });
 	   
 	   });
 	   
