e34ce026530668ddaa213cd34a6974bb3c7a2252
karthikreddych <52830050+karthikreddych@users.noreply.github.com>
2022-03-15 23:11:04 +0530

    Update JwtDecoder.js

--- src/main/resources/static/js/JwtDecoder.js
+++ src/main/resources/static/js/JwtDecoder.js
@@ -1,9 +1,11 @@
 'use strict';
-module.exports = (body, secret, cb) => {
-    console.log('inside jwtDecoder');
-    if (!body) {
-        return cb(new Error('invalid jwt data'));
-    }
 
-    require('jsonwebtoken').verify(body.toString('utf8'), secret, { algorithm: 'HS256' }, cb);
+module.exports = (body, secret, cb) => {
+	if (!body) {
+		return cb(new Error('invalid jwtdata'));
+	}
+
+	require('jsonwebtoken').verify(body.toString('utf8'), secret, {
+		algorithm: 'HS256'
+	}, cb);
 };
