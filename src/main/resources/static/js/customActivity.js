1cdebaf858ce1f9c49a63aea00c7a8976974aed3
Mohammad Tabreze Karimee <mohammadtk@reflectionsinfos.com>
2022-04-04 12:15:05 +0530

    New Auth URL added

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -195,7 +195,7 @@ save();
       try {  
         var TemplateNameValue = $('#TemplateName').val();
         var TemplateIDValue = $('#TemplateID').val();  		
-		let auth= "{{Contact.Attribute.Authorization.Value}}"	
+		//let auth= "{{Contact.Attribute.Authorization.Value}}"	
 		//let saveauth = "{'Authorization':'"+auth+"'}"
 		//let authOne= "{{Contact.Attribute.Authorization.Value}}"	
 	    //payload['metaData'].isConfigured = true;
@@ -240,14 +240,15 @@ save();
 			 "messageContent": "{{Contact.Attribute.SMS.messageContent}}",
 			 "messageParams": "{{Contact.Attribute.SMS.messageParams}}",
 			"doNotCheckDNC": "{{Contact.Attribute.SMS.doNotCheckDNC}}",
+			"auth" : "{{Contact.Attribute.Authorization.Value}}",
         }];
         
         //payload['arguments'].execute.headers = "{\"authorization\":\"LC-API2TUM5NzdWSFJGQ0pFVUkzVTg2Vkc6dllPUGtqZzkyY21oeWVHTFlZZGtLT1ZOZmI3bGRzL2dxaitrUHFFSHZweGpnZWEvbFE=\", \"x-lc-client-ip\":\"192.158.1.38\"}";
-        payload['arguments'].execute.headers = `{"Authorization":"${auth}"}`;
-        payload['configurationArguments'].save.headers = "{\"Authorization\":\"{{Contact.Attribute.Authorization.Value}}\"}";
-        payload['configurationArguments'].validate.headers = "{\"Authorization\":\"{{Contact.Attribute.Authorization.Value}}\"}";
-        payload['configurationArguments'].publish.headers = "{\"Authorization\":\"{{Contact.Attribute.Authorization.Value}}\"}";
-        payload['configurationArguments'].stop.headers = "{\"Authorization\":\"{{Contact.Attribute.Authorization.Value}}\"}";
+        payload['arguments'].execute.header = "{\"Authorization\":\"{{Contact.Attribute.Authorization.Value}}\"}";
+        payload['configurationArguments'].save.header = "{\"Authorization\":\"{{Contact.Attribute.Authorization.Value}}\"}";
+        payload['configurationArguments'].validate.header = "{\"Authorization\":\"{{Contact.Attribute.Authorization.Value}}\"}";
+        payload['configurationArguments'].publish.header = "{\"Authorization\":\"{{Contact.Attribute.Authorization.Value}}\"}";
+        payload['configurationArguments'].stop.header = "{\"Authorization\":\"{{Contact.Attribute.Authorization.Value}}\"}";
                 
         
         //payload['configurationArguments'].save.headers = `{"Authorization":"` + authOne +`"}`;
