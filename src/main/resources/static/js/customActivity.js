6d5af7d939f9de078868ed2ea3020d0b202e4633
karthikreddych <52830050+karthikreddych@users.noreply.github.com>
2022-08-07 10:39:48 +0530

    Update customActivity.js

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -198,7 +198,7 @@ payload['arguments'].execute.inArguments = [{
           
             payload['metaData'].isConfigured = true;
 
-            console.log("payload"+payload);
+            console.log("payload"+JSON.stringify(payload));
             connection.trigger('updateActivity', payload);
 
         } catch (err) {
