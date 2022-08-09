6a0402364aecc63f4f58333cb2a8e8fc0bcb74e0
karthikreddych <52830050+karthikreddych@users.noreply.github.com>
2022-08-09 17:18:43 +0530

    Update customActivity.js

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -164,7 +164,7 @@ payload['arguments'].execute.inArguments = [{
                 "jobDescription": "{{Event." + eventDefinitionKey + '."jobDescription"}}',
                 "loanId" : "{{Event." + eventDefinitionKey + '."loanId"}}',
                 "actorId": "{{Event." + eventDefinitionKey + '."actorId"}}',
-                "categoryName": TemplateNameValue,
+                "categoryName": "('{{Contact.Attribute.PNMSMSDE.eventType}}'=='null' || '{{Contact.Attribute.PNMSMSDE.eventType}}'=='') ?"+TemplateNameValue+": 'Test'",
                 "messageContent": "{{Event." + eventDefinitionKey + '."messageContent"}}',
                 "source": "{{Event." + eventDefinitionKey + '."source"}}',
                 "sourceMessageId": "{{Event." + eventDefinitionKey + '."sourceMessageId"}}',
